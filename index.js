bfddc9d8b692ba0c11c80795ffbde9b33d2dabdc
Job Ferreira <65796024+jobinferreira@users.noreply.github.com>
2023-10-16 21:00:49 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ let expHeroi = Math.random() * 10000;
 function heroLevel(expHeroi) {
     if (expHeroi < 1000) {
         expHeroi = 'Ferro'
-    } else if (expHeroi < 1001 && expHeroi < 2000) {
+    } else if (expHeroi > 1001 && expHeroi < 2000) {
         expHeroi = 'Bronze'
-    } else if (expHeroi < 2001 && expHeroi < 5000) {
+    } else if (expHeroi > 2001 && expHeroi < 5000) {
         expHeroi = 'Prata'
-    } else if (expHeroi < 6001 && expHeroi < 7000) {
+    } else if (expHeroi > 6001 && expHeroi < 7000) {
         expHeroi = 'Ouro'
-    } else if (expHeroi < 7001 && expHeroi < 8000) {
+    } else if (expHeroi > 7001 && expHeroi < 8000) {
         expHeroi = 'Platina'
-    } else if (expHeroi < 8001 && expHeroi < 9000) {
+    } else if (expHeroi > 8001 && expHeroi < 9000) {
         expHeroi = 'Ascendente'
-    } else if (expHeroi < 9001 && expHeroi < 10000) {
+    } else if (expHeroi > 9001 && expHeroi < 10000) {
         expHeroi = 'Imortal'
     } else if (expHeroi >= 10000) {
         expHeroi = 'Radiante'
